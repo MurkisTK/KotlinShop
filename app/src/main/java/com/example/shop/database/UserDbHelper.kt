483a0a0d15fd20fb67ced1fc1812742faa51d7db
murkis <timkisart@gmail.com>
2025-11-26 20:58:56 +0300

    update user db helper

--- app/src/main/java/com/example/shop/database/UserDbHelper.kt
+++ app/src/main/java/com/example/shop/database/UserDbHelper.kt
@@ -8,10 +8,27 @@ import com.example.shop.models.User
 
 class UserDbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, "app", factory, 1) {
+    companion object {
+        const val TABLE_NAME = "users"
+        const val COLUMN_ID = "id"
+        const val COLUMN_LOGIN = "login"
+        const val COLUMN_EMAIL = "email"
+        const val COLUMN_PHONE = "phone"
+        const val COLUMN_PASSWORD = "password"
+    }
     override fun onCreate(db: SQLiteDatabase?) {
-        val query =
-            "CREATE TABLE users (id INT PRIMARY KEY, login TEXT, email TEXT, phone TEXT, password TEXT)"
+        val query = """
+            CREATE TABLE $TABLE_NAME (
+                $COLUMN_ID INT PRIMARY KEY, 
+                $COLUMN_LOGIN TEXT,
+                $COLUMN_EMAIL TEXT,
+                $COLUMN_PHONE TEXT,
+                $COLUMN_PASSWORD TEXT
+            )
+        """.trimIndent()
         db!!.execSQL(query)
+
+        addUser(User("a", "a", "a", "a"))
     }
 
     override fun onUpgrade(
@@ -24,25 +41,61 @@ class UserDbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         onCreate(db)
     }
 
+    fun getAllUsers(): ArrayList<User> {
+        val users = mutableListOf<User>()
+        val db = readableDatabase
+
+        val cursor = db.query(
+            TABLE_NAME, null, null, null, null, null, null
+        )
+
+        cursor.use { c ->
+            val loginIndex = c.getColumnIndex(COLUMN_LOGIN)
+            val emailIndex = c.getColumnIndex(COLUMN_EMAIL)
+            val passwordIndex = c.getColumnIndex(COLUMN_PASSWORD)
+            val phoneIndex = c.getColumnIndex(COLUMN_PHONE)
+
+            while (c.moveToFirst()) {
+                val user = User(
+                    c.getString(loginIndex),
+                    c.getString(emailIndex),
+                    c.getString(passwordIndex),
+                    c.getString(phoneIndex),
+                )
+                users.add(user)
+            }
+        }
+
+        db.close()
+        return users as ArrayList<User>
+    }
+
     fun addUser(user: User) {
         val content = ContentValues()
-        content.put("login", user.login)
-        content.put("email", user.email)
-        content.put("phone", user.phone)
-        content.put("password", user.password)
+        content.put(COLUMN_LOGIN, user.login)
+        content.put(COLUMN_EMAIL, user.email)
+        content.put(COLUMN_PHONE, user.phone)
+        content.put(COLUMN_PASSWORD, user.password)
 
         val db = this.writableDatabase
-        db.insert("users", null, content)
+        db.insert(TABLE_NAME, null, content)
 
         db.close()
     }
 
     fun isUserExist(login: String, password: String): Boolean {
         val db = this.readableDatabase
+        val selection = "$COLUMN_LOGIN = ? AND $COLUMN_PASSWORD = ?"
+        val selectionArgs = arrayOf(login, password)
 
-        val query = db.rawQuery(
-            "SELECT * FROM users WHERE password = '$password' AND login = '$login'",
-            null
+        val query = db.query(
+            TABLE_NAME,
+            null,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            null,
         )
         val result = query.moveToFirst()
 
@@ -55,7 +108,7 @@ class UserDbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     fun deleteUser(login: String) {
         val db = this.writableDatabase
 
-        db.execSQL("DELETE FROM users WHERE login = '$login'")
+        db.execSQL("DELETE FROM $TABLE_NAME WHERE $COLUMN_LOGIN = '$login'")
 
         db.close()
     }
