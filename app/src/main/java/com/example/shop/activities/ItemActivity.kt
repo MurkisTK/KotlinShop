12885e4bd8e1ad19617359b0c59916b6f169bc8a
murkis <timkisart@gmail.com>
2025-11-26 03:35:46 +0300

    update ItemActivity.kt

--- app/src/main/java/com/example/shop/activities/ItemActivity.kt
+++ app/src/main/java/com/example/shop/activities/ItemActivity.kt
@@ -11,25 +11,36 @@ import com.example.shop.R
 import kotlin.collections.get
 
 class ItemActivity : AppCompatActivity() {
+    private lateinit var title: TextView
+    private lateinit var desc: TextView
+    private lateinit var price: TextView
+    private lateinit var buyButton: Button
+    private lateinit var image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item)
 
-        val title: TextView = findViewById(R.id.title)
-        val desc: TextView = findViewById(R.id.description)
-        val price: TextView = findViewById(R.id.price)
-        val buyButton: Button = findViewById(R.id.buy_button)
-        val image: ImageView = findViewById(R.id.image)
+        initialize()
+
+        val priceValue = intent.getIntExtra("price", 0)
 
         title.text = intent.getStringExtra("title")
-        price.text = getString(R.string.price, intent.getIntExtra("price", 0).toString())
+        price.text = getString(R.string.price, priceValue.toString())
         desc.text = intent.getStringExtra("desc")
-
         image.setImageResource(ImageMapper.images[intent.getStringExtra("image")]!!)
 
         buyButton.setOnClickListener {
             val intent = Intent(this, ItemsActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun initialize() {
+        title = findViewById(R.id.title)
+        desc = findViewById(R.id.description)
+        price = findViewById(R.id.price)
+        buyButton = findViewById(R.id.buy_button)
+        image = findViewById(R.id.image)
     }
 }
