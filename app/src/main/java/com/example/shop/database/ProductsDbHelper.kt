4d5e44df184c36e295c8e9dfd553d10c35048637
murkis <timkisart@gmail.com>
2025-11-26 20:34:28 +0300

    update products db helper

--- app/src/main/java/com/example/shop/database/ProductsDbHelper.kt
+++ app/src/main/java/com/example/shop/database/ProductsDbHelper.kt
@@ -8,8 +8,28 @@ import com.example.shop.models.Item
 
 class ProductsDbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, "app", factory, 1) {
+    companion object {
+        const val TABLE_NAME = "products"
+        const val COLUMN_ID = "id"
+        const val COLUMN_PRICE = "price"
+        const val COLUMN_IMAGE = "image"
+        const val COLUMN_DESCRIPTION = "description"
+        const val COLUMN_TEXT = "text"
+        const val COLUMN_TITLE = "title"
+    }
+
     override fun onCreate(db: SQLiteDatabase?) {
-        val query = "CREATE TABLE products (id INT PRIMARY KEY, image TEXT, title TEXT, description TEXT, text TEXT, price INT)"
+        val query = """
+            CREATE TABLE $TABLE_NAME (
+                $COLUMN_ID INT PRIMARY KEY, 
+                $COLUMN_IMAGE TEXT,
+                $COLUMN_TITLE TEXT, 
+                $COLUMN_DESCRIPTION TEXT, 
+                $COLUMN_TEXT TEXT, 
+                $COLUMN_PRICE INT
+            )
+        """.trimIndent()
+
         db!!.execSQL(query)
         addItem(Item(1, "i1", "t1", "d1", "text1", 100))
         addItem(Item(2, "i2", "t2", "d2", "text2", 200))
@@ -17,34 +37,88 @@ class ProductsDbHelper(context: Context, factory: SQLiteDatabase.CursorFactory?)
     }
 
     override fun onUpgrade(
-        db: SQLiteDatabase?,
-        oldVersion: Int,
-        newVersion: Int
+        db: SQLiteDatabase?, oldVersion: Int, newVersion: Int
     ) {
         db!!.execSQL("DROP TABLE IF EXISTS products")
 
         onCreate(db)
     }
 
-    fun getItemById(id: Int){
+    fun getItemById(id: Int): Item? {
+        val db = readableDatabase
+        val selection = "$COLUMN_ID = ?"
+        val selectionArgs = arrayOf(id.toString())
 
+        val cursor = db.query(
+            TABLE_NAME, null, selection, selectionArgs, null, null, null
+        )
+        cursor.use { c ->
+            if (c.moveToFirst()) {
+                val idIndex = c.getColumnIndex(COLUMN_ID)
+                val textIndex = c.getColumnIndex(COLUMN_TEXT)
+                val titleIndex = c.getColumnIndex(COLUMN_TITLE)
+                val descIndex = c.getColumnIndex(COLUMN_DESCRIPTION)
+                val priceIndex = c.getColumnIndex(COLUMN_PRICE)
+                val imageIndex = c.getColumnIndex(COLUMN_IMAGE)
+
+                return Item(
+                    c.getInt(idIndex),
+                    c.getString(imageIndex),
+                    c.getString(titleIndex),
+                    c.getString(descIndex),
+                    c.getString(textIndex),
+                    c.getInt(priceIndex),
+                )
+            } else {
+                return null
+            }
+        }
     }
 
-    fun getAllItems(): ArrayList<Item>{
-        return arrayListOf()
+    fun getAllItems(): ArrayList<Item> {
+        val items = mutableListOf<Item>()
+        val db = readableDatabase
+
+        val cursor = db.query(
+            TABLE_NAME, null, null, null, null, null, null
+        )
+
+        cursor.use { c ->
+            val idIndex = c.getColumnIndex(COLUMN_ID)
+            val textIndex = c.getColumnIndex(COLUMN_TEXT)
+            val titleIndex = c.getColumnIndex(COLUMN_TITLE)
+            val descIndex = c.getColumnIndex(COLUMN_DESCRIPTION)
+            val priceIndex = c.getColumnIndex(COLUMN_PRICE)
+            val imageIndex = c.getColumnIndex(COLUMN_IMAGE)
+
+            while (c.moveToFirst()) {
+                val item = Item(
+                    c.getInt(idIndex),
+                    c.getString(imageIndex),
+                    c.getString(titleIndex),
+                    c.getString(descIndex),
+                    c.getString(textIndex),
+                    c.getInt(priceIndex),
+                )
+                items.add(item)
+            }
+        }
+
+        db.close()
+        return items as ArrayList<Item>;
     }
 
     fun addItem(item: Item) {
         val content = ContentValues()
-        content.put("title", item.title)
-        content.put("id", item.id)
-        content.put("price", item.price)
-        content.put("image", item.image)
-        content.put("desc", item.desc)
-        content.put("text", item.text)
+        content.put(COLUMN_TITLE, item.title)
+        content.put(COLUMN_ID, item.id)
+        content.put(COLUMN_PRICE, item.price)
+        content.put(COLUMN_IMAGE, item.image)
+        content.put(COLUMN_DESCRIPTION, item.desc)
+        content.put(COLUMN_TEXT, item.text)
 
         val db = this.writableDatabase
-        db.insert("products", null, content)
+        db.insert(TABLE_NAME, null, content)
 
         db.close()
     }
