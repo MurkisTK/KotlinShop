a4ca66f4351678a37b5c6107d8ad7f5a41c720b8
murkis <timkisart@gmail.com>
2025-11-26 20:58:12 +0300

    update code struct in items activity

--- app/src/main/java/com/example/shop/activities/ItemsActivity.kt
+++ app/src/main/java/com/example/shop/activities/ItemsActivity.kt
@@ -9,26 +9,33 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.shop.models.Item
 import com.example.shop.adapters.ItemAdapter
 import com.example.shop.R
+import com.example.shop.database.ProductsDbHelper
+import org.w3c.dom.Text
 
 class ItemsActivity : AppCompatActivity() {
+    private lateinit var usersLink: TextView;
+    private lateinit var itemsList: RecyclerView;
+    private lateinit var items: ArrayList<Item>;
+    private var dbHelper: ProductsDbHelper = ProductsDbHelper(this, null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items)
 
-        val usersLink: TextView = findViewById(R.id.users_link)
-        val itemsList = findViewById<RecyclerView>(R.id.items_list)
-        val items = arrayListOf<Item>()
-
-        items.add(Item(1, "i1", "t1", "d1", "text1", 100))
-        items.add(Item(2, "i2", "t2", "d2", "text2", 200))
-        items.add(Item(3, "i3", "t3", "d3", "text3", 300))
-
-        itemsList.layoutManager = LinearLayoutManager(this)
-        itemsList.adapter = ItemAdapter(items, this)
+        initialize()
 
         usersLink.setOnClickListener {
             val intent = Intent(this, UsersActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun initialize(){
+        usersLink = findViewById(R.id.users_link)
+        itemsList = findViewById<RecyclerView>(R.id.items_list)
+        items = dbHelper.getAllItems()
+
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = ItemAdapter(items, this)
     }
 }
